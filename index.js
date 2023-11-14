6c2133dd92db3a5f732952145943a01d33af87f6
Christopher Vu <christophervu4@gmail.com>
2023-11-13 22:07:26 -0800

    background adjustments

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-// boilerplate sleep constant
-const sleep = (milliseconds) => {
-  return new Promise(resolve => setTimeout(resolve, milliseconds))
-}
-
-// greeting
-async function greeting() {
-    window.scrollTo(0,0); // bring user to top of page
-    
-    const greeting = document.getElementById("greeting");
-    const greetingText = document.getElementById("greeting-text");
-
-    const delays = [350, 250, 150, 120, 120, 120, 120, 120, 120, 120, 120];
-    const words = ["Xin chào", "привет", "你好", "नमस्ते", "こんにちは", "Hola", "Guten Tag", "سلام", "Ciao", "Salve", "안녕하세요",];
-    
-    await sleep(600);
-    for (let i = 0; i < delays.length; i++) {
-        await sleep(delays[i]);
-        greetingText.textContent = words[i];
-    }
-    await sleep(500);
-
-    window.scrollTo(0,0); // bring user to top of page
-
-    // pull greeting from view
-    greeting.style.transform = "translateY(-105vh)";
-    greeting.style.borderBottomLeftRadius = "100vw";
-    greeting.style.borderBottomRightRadius = "100vw";
-}
-
-greeting();
-
-
 document.addEventListener('scroll', function() {
     // get scroll position
     var scrollPosition = window.scrollY;
---
+++ landing.js
@@ -0,0 +1,37 @@
+// boilerplate sleep constant
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+// greeting
+async function greeting() {
+    window.scrollTo(0,0); // bring user to top of page
+    
+    const greeting = document.getElementById("greeting");
+    const greetingText = document.getElementById("greeting-text");
+
+    const delays = [350, 250, 150, 120, 120, 120, 120, 120, 120, 120, 120];
+    const words = ["Xin chào", "привет", "你好", "नमस्ते", "こんにちは", "Hola", "Guten Tag", "سلام", "Ciao", "Salve", "안녕하세요",];
+    
+    await sleep(600);
+    for (let i = 0; i < delays.length; i++) {
+        await sleep(delays[i]);
+        greetingText.textContent = words[i];
+    }
+    await sleep(500);
+
+    window.scrollTo(0,0); // bring user to top of page
+
+    // pull greeting from view
+    greeting.style.transform = "translateY(-105vh)";
+    greeting.style.borderBottomLeftRadius = "100vw";
+    greeting.style.borderBottomRightRadius = "100vw";
+}
+
+greeting();
+
+
+
+// grid looking thingy
+let columns = Math.floor(document.body.clientWidth / 50), 
+    rows = Math.floor(document.body.clientHeight/ 50); 
