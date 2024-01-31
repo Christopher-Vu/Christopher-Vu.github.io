80260f9fba2117c9ab93b79e92cae254ab7d6efb
Christopher Vu <christophervu4@gmail.com>
2024-01-30 22:17:35 -0800

    some changes, finalized landing

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ document.addEventListener('scroll', function() {
     const navbar = document.getElementById('navbar');
 
     // Check if user has scrolled > 70vh, and activate navbar line if so
-    if (scrollPosition > 0.2 * window.innerHeight) { 
+    if (scrollPosition > 0.3 * window.innerHeight) { 
         navBorder.style.width = '100%';
     } 
     
-    if (scrollPosition < 0.2 * window.innerHeight) { 
+    if (scrollPosition < 0.3 * window.innerHeight) { 
         navBorder.style.width = '0';
     }
   });
