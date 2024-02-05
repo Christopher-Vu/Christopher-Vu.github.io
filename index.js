ac8d623c92b1f110e4e143f7cf03e4a812459093
Christopher Vu <christophervu4@gmail.com>
2024-02-04 17:29:47 -0800

    secondary bg colors

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ document.addEventListener('scroll', function() {
 
 var bgColors = ['#E0CCBE', '#000000', '#EEEDEB', '#17223B', '#CFD3CE'];
 var textColors = ['#3C3633', '#F0EDCF','#DA2D2D', '#FC6736', '#34626C'];
-var emphasisColors = ['#2D2926', '#F4F3E0', '#9F0C0C', '#E94324', '#1D3C46'];
+var emphasisColors = ['#2F2A28', '#FFFFFF', '#2A0E0E', '#FF8969', '#486A73'];
+var secondaryBgColors = ['#C8BAA9', '#1A1A1A', '#D5D4D3', '#1B2435', '#A6A9A8'];
 var bgUrls = ["landing-img1.png", "landing-img2.png", "landing-img3.png", "landing-img4.png", "landing-img5.png"];
 var bgPos = ["top", "top", "center", "top", "center"];
 let colInd = 0;
@@ -30,6 +31,7 @@ function swapColors() {
     document.documentElement.style.setProperty('--main-bg-color', bgColors[colInd]);
     document.documentElement.style.setProperty('--main-text-color', textColors[colInd]);
     document.documentElement.style.setProperty('--emphasis-text', emphasisColors[colInd]);
+    document.documentElement.style.setProperty('--secondary-bg-color', secondaryBgColors[colInd]);
     let landingImg = document.getElementById("landing-img");
     landingImg.style.setProperty('background-image', 'url("static/' + bgUrls[colInd] + '")'); 
     landingImg.style.setProperty('background-position', bgPos[colInd]);
